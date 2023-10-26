b8e32fcafd34722ff3b0006c01354b03bfc65933
GNRicardo <149065649+GNRicardo@users.noreply.github.com>
2023-10-26 12:20:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, spinach & Rocket Oatmeal')
 streamlit.text('🐔 Hard-Boilde Free-Range Egg')
 streamlit.text('🥑🍞 Advocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
