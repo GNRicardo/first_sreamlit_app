0454aac9d757738c0ad7107b66725ec1688552f0
GNRicardo <149065649+GNRicardo@users.noreply.github.com>
2023-10-26 12:13:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subtitle('Beakfast Menu')
+streamlit.header('Beakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
