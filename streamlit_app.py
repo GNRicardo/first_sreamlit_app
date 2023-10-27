a5c4d6f4253721e2d5388465f795bf6fd16f4a75
GNRicardo <149065649+GNRicardo@users.noreply.github.com>
2023-10-27 16:00:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,10 @@ streamlit.header('Fruityvice Fruit Advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
-
-# write your own comment -what does the next line do? 
 # Add into a array all the dimensons
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+
 # create a table with all the elements in jason  
 streamlit.dataframe(fruityvice_normalized)
